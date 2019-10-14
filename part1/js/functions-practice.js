7ba5f0230bfed58b6babcdf55f3848f8a0bf337d
JaySDSU <54603632+JaySDSU@users.noreply.github.com>
2019-10-13 23:21:56 -0700

    some changes
    Changed function

--- part1/js/functions-practice.js
+++ part1/js/functions-practice.js
@@ -2,31 +2,32 @@
 //STEP 1
 halfNumber = function(number) {
     "use strict";
-    result = float(number / 2);
+    var result;
+    result = (number / 2);
     window.console.log("Half of " + number + " is " + result + ".<br>");
-    return result
 };
+
 //STEP 2
 squareNumber = function(number) {
     "use strict";
+    var result;
     result = number * number;
     window.console.log("The result of squaring the number " + number + " is " + result + ".<br>");
-    return result
 };
 //STEP 3
 percentOf = function(number1, number2) {
     "use strict";
+    var result;
     result = (number1 / number2) * 100;
     window.console.log(number1 + " is " + result + "% of " + number2 + ".<br>");
-    return result
 };
 
 //STEP 4
 findModulus = function(number1, number2) {
     "use strict";
+    var result;
     result = number1 % number2;
     window.console.log(result + " is the modulus of " + number1 + " and " + number2 + ".<br>");
-    return result
 }
 
 //STEP 5
@@ -40,5 +41,9 @@ sum = function(myArray) {
     window.console.log(result);
 }
 
+halfNumber(10);
+squareNumber(10);
+percentOf(4, 2);
+findModulus(10, 6);
 myArray = window.prompt("Enter the numbers").split(',');
 sum(myArray);
